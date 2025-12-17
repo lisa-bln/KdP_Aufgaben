06c5bebcd5914614e98729652271b12ceab34175
Lisa <lisa.zemlin@proton.me>
2025-12-17 21:59:36 +0100

    Aufgabe 4 a), b) und c) bearbeitet. Spezifikationen sind drin aber die Funktionen sind noch nicht ausreichend getestet.

---
+++ funktionenHöhererOrdnung.scala
@@ -0,0 +1,42 @@
+//a) 
+/*
+Vor: Eingabeliste besteht aus Integern
+Eff: -
+Erg: Liste in der die Einträge der Eingabe verdoppelt wurden. 
+*/
+
+def doubleList(list: List[Int]): List[Int] = 
+  list.map(_*2)
+
+
+//b)
+/*
+Vor: Die Liste aus Charaktern besteht ausschließlich aus Buchstaben auf welche die Funktion _.isUpper angewendet werden kann.
+Eff: - 
+Erg: Eine Liste, die alle Kleinbuchstaben der Eingabe enthält.
+*/
+
+def withoutUpper(list: List[Char]): List[Char] =
+  list match
+  case Nil => Nil
+  case x :: xs if (! x.isUpper) => x :: withoutUpper(xs)
+  case x :: xs => withoutUpper(xs)
+
+//c)
+/*
+Vor: l1 und l2 sind vom Typ List[Int] und f eine Funktion von (Int, Int) => Int
+Eff: - 
+Erg: Liste mit Wert f(l1(i), l2(i)) an Position i. Ist eine Liste länger wird der Rest der längeren Liste verworfen.
+*/
+
+def zipWith(f: (Int, Int) => Int, l1: List[Int], l2: List[Int]): List[Int] =
+  (l1, l2) match
+  case (x :: xs, y :: ys) => f(x, y) :: zipWith(f, xs, ys)
+  case _ => Nil
+
+@main def run(): Unit =
+  println(doubleList(List(1, 2, 3)))
+  println(withoutUpper(List('a', 'A', 'b', 'B')))
+  println(zipWith((_+_), List(1, 2, 3), List(3, 2, 1, 4)))
+
+
